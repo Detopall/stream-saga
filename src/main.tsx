292859ececd94f2b662f2c2384c80f17b46f5e89
denis.topallaj <denis.topallaj@student.howest.be>
2023-08-31 23:06:20 +0200

    add: basic routing system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</>
+);

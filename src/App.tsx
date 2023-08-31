101c8259f284240d4f5b801c338247850f8d8f69
denis.topallaj <denis.topallaj@student.howest.be>
2023-08-31 23:08:36 +0200

    add: basic nav system

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-
+import Nav from "./components/Nav";
 function App() {
 
 	return (
 		<div className="App">
+			<Nav />
+
 			<Routes>
 				<Route path="/" element={<h1>Home</h1>} />
 				<Route path="/about" element={<h1>About</h1>} />
